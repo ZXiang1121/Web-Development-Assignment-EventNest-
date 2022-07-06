3e1ea1e8ef46ef4489674b094adc582c23e937d6
Zhang Xiang <zhang xiang1121@gmail.com>
2022-07-06 22:14:16 +0800

    create event filter & finalise navbar

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/ticketdetails')
+def ticketdetails():
+    return render_template('ticketdetails.html')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
