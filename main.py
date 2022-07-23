293bd9a3a92906dc8b88c554642de7791500741b
Parik <210895h@mymail.nyp.edu.sg>
2022-07-23 18:46:54 +0800

    add my new dash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from Form import CreateEventForm
-import shelve, Event
 
 app = Flask(__name__)
 
@@ -53,44 +51,6 @@ def dashboard():
 
 
 
-@app.route('/createEvent', methods = ['GET', 'POST'])
-def createEvent():
-    # create_event_form = CreateEventForm(request.form)
-
-    # if request.method == 'POST' and create_event_form.validate():
-    #     events_dict = {}
-    #     db = shelve.open('storage.db', 'c')
-
-    #     try:
-    #         events_dict = db['Events']
-    #     except:
-    #         print("Error in retrieving Users from storage.db.")
-        
-    #     event = Event.Event(create_event_form.event_name.data,
-    #                         create_event_form.num_ticket.data,
-    #                         create_event_form.event_price.data,
-    #                         create_event_form.seat_type.data,
-    #                         create_event_form.event_date.data,
-    #                         # create_event_form.event_image.data,
-    #                         create_event_form.description.data
-    #                         )
-    #     events_dict[event.get_event_name()] = event
-    #     db['Events'] = events_dict
-        
-    #     #Test Code
-    #     events_dict = db['Events']
-    #     event = events_dict[event.get_event_name()]
-    #     print(event.get_event_location(), event.get_event_date(), "was stored in storage.db successfully with event_name ==", event.get_event_name)
-
-    #     db.close()
-
-
-    #     return redirect(url_for('home'))
-    return render_template('createEvent.html', form=create_event_form)
-
-@app.route('/myevent')
-def myEvent():
-    return render_template('myevent.html')   
 
 if __name__ == '__main__':
     app.run(debug=True)
